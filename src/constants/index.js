a1be178fc13b6e5270b0f4d4fe550aa1c3fcae26
andriiVovnenko1992 <vovnenkoandrii@gmail.com>
2021-03-27 18:13:04 +0200

    update end

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.REACT_APP_ENV === 'production' ? 'http://8.211.2.107/' : 'http://localhost:3001/';
+export const baseUrl = process.env.REACT_APP_ENV === 'production' ? 'http://8.211.2.107/api/' : 'http://localhost:3001/';
